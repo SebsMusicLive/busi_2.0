02b50c3344a3598e7e35fa3dd205b661baa472a0
Sebs_music_live <lopezortegajohansebastian@gmail.com>
2024-11-11 17:22:14 -0500

    Se agregan modificaciones y componentes

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,11 +3,13 @@ import { RouterOutlet } from '@angular/router';
 import { NavBarComponent } from "./nav-bar/nav-bar.component";
 import { FooterComponent } from "./footer/footer.component";
 import { InicioComponent } from "./inicio/inicio.component";
+import { SobreNosotrosComponent } from "./sobre-nosotros/sobre-nosotros.component";
+import { WhatsappComponent } from "./whatsapp/whatsapp.component";
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet, NavBarComponent, FooterComponent, InicioComponent],
+  imports: [NavBarComponent, FooterComponent, InicioComponent, SobreNosotrosComponent, WhatsappComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
