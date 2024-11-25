63d87dfa3a494a3f1a273aee1c25705a3b3adc70
Sebs_music_live <lopezortegajohansebastian@gmail.com>
2024-11-24 23:52:29 -0500

    Se agregan nuevos componentes y se solucionan problemas

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,11 +6,12 @@ import { InicioComponent } from "./inicio/inicio.component";
 import { SobreNosotrosComponent } from "./sobre-nosotros/sobre-nosotros.component";
 import { WhatsappComponent } from "./whatsapp/whatsapp.component";
 import { ContactanosComponent } from "./contactanos/contactanos.component";
+import { EstadisticasComponent } from "./estadisticas/estadisticas.component";
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [NavBarComponent, FooterComponent, InicioComponent, SobreNosotrosComponent, WhatsappComponent, ContactanosComponent],
+  imports: [NavBarComponent, FooterComponent, InicioComponent, SobreNosotrosComponent, WhatsappComponent, ContactanosComponent, EstadisticasComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
